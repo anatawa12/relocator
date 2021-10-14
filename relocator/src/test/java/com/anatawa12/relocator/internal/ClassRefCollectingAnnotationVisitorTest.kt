e567d10b40d9f765f20a517fc49476698a33e8d3
anatawa12 <anatawa12@icloud.com>
2021-10-14 21:27:31 +0900

    add Test for ClassRefCollectingAnnotationVisitor

---
+++ relocator/src/test/java/com/anatawa12/relocator/internal/ClassRefCollectingAnnotationVisitorTest.kt
@@ -0,0 +1,72 @@
+package com.anatawa12.relocator.internal
+
+import org.amshove.kluent.*
+import org.junit.jupiter.api.Test
+import org.objectweb.asm.Type
+import org.objectweb.asm.tree.AnnotationNode
+
+internal class ClassRefCollectingAnnotationVisitorTest {
+    fun test(annotation: AnnotationNode): Set<ClassReference> {
+        val env = TestingComputeReferenceEnvironment()
+        val collection = hashSetOf<ClassReference>()
+        annotation.accept(ClassRefCollectingAnnotationVisitor(collection, env))
+        return collection
+    }
+
+    @Test
+    fun noValueAnnotation() {
+        test(AnnotationNode("Ljava/lang/annotation/Documented;"))
+            .`should be equal to`(setOf(
+                ClassReference("java/lang/annotation/Documented"),
+            ))
+    }
+
+    @Test
+    fun enumValueAnnotation() {
+        test(AnnotationNode("Ljava/lang/annotation/Retention;").apply {
+            visitEnum("value", "Ljava/lang/annotation/RetentionPolicy;", "CLASS")
+        })
+            .`should be equal to`(setOf(
+                ClassReference("java/lang/annotation/Retention"),
+                ClassReference("java/lang/annotation/RetentionPolicy"),
+            ))
+        test(AnnotationNode("Ljava/lang/annotation/Target;").apply {
+            visitArray("value").apply {
+                visitEnum(null, "Ljava/lang/annotation/ElementType;", "TYPE")
+            }
+        })
+            .`should be equal to`(setOf(
+                ClassReference("java/lang/annotation/Target"),
+                ClassReference("java/lang/annotation/ElementType"),
+            ))
+    }
+
+    @Test
+    fun classValueAnnotation() {
+        /*
+        package com.anatawa12.relocator;
+        @interface TestAnnotation {
+             Class<?> value() default Object.class;
+             Class<?>[] values() default {};
+        }
+         */
+        test(AnnotationNode("Lcom/anatawa12/relocator/TestAnnotation;").apply {
+            visit("value", Type.getType("Ljava/lang/String;"))
+        })
+            .`should be equal to`(setOf(
+                ClassReference("com/anatawa12/relocator/TestAnnotation"),
+                ClassReference("java/lang/String"),
+            ))
+        test(AnnotationNode("Lcom/anatawa12/relocator/TestAnnotation;").apply {
+            visitArray("values").apply {
+                visit(null, Type.getType("Ljava/lang/String;"))
+                visit(null, Type.getType("[Ljava/lang/annotation/ElementType;"))
+            }
+        })
+            .`should be equal to`(setOf(
+                ClassReference("com/anatawa12/relocator/TestAnnotation"),
+                ClassReference("java/lang/String"),
+                ClassReference("java/lang/annotation/ElementType"),
+            ))
+    }
+}
