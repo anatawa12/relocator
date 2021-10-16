11b8f5b153df74633ef155558c488cd359f36245
anatawa12 <anatawa12@icloud.com>
2021-10-16 14:00:04 +0900

    rewrite Relocator in kotlin

--- relocator/src/main/java/com/anatawa12/relocator/Relocator.kt
+++ relocator/src/main/java/com/anatawa12/relocator/Relocator.kt
@@ -1,67 +1,55 @@
-package com.anatawa12.relocator;
+package com.anatawa12.relocator
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.function.Function;
+import java.io.File
+import java.util.*
+import java.util.function.Function
 
-import static java.util.Objects.requireNonNull;
-
-public class Relocator {
+class Relocator {
     /**
      * The list of jar files/directories which has omit-able classes.
      */
-    private final List<File> classPath = new ArrayList<>();
+    private val classPath: MutableList<File> = ArrayList()
 
     /**
      * Add a file or directory to classpath whose classes may be omitted.
      * @param classPath The path to file or directory to classpath.
-     *                  The classes contain in the classpath may be omitted.
+     * The classes contain in the classpath may be omitted.
      */
-    public void addClassPath(File classPath) {
-        requireNonNull(classPath, "classPath");
-        this.classPath.add(classPath);
+    fun addClassPath(classPath: File) {
+        this.classPath.add(classPath)
     }
 
-    public List<File> getClassPath() {
-        return Collections.unmodifiableList(classPath);
+    fun getClassPath(): List<File> {
+        return Collections.unmodifiableList(classPath)
     }
 
     /**
      * The list of jar files/directories which has un-omit-able classes.
      */
-    private final List<File> rootPath = new ArrayList<>();
+    private val rootPath: MutableList<File> = ArrayList()
 
     /**
      * Add a file or directory to classpath whose classes never be omitted.
      * @param classPath The path to file or directory to classpath.
-     *                  The classes contain in this classpath never be omitted.
+     * The classes contain in this classpath never be omitted.
      */
-    public void addRootClassPath(File classPath) {
-        requireNonNull(classPath, "classPath");
-        this.rootPath.add(classPath);
+    fun addRootClassPath(classPath: File) {
+        rootPath.add(classPath)
     }
 
-    public List<File> getRootPath() {
-        return Collections.unmodifiableList(rootPath);
+    fun getRootPath(): List<File> {
+        return Collections.unmodifiableList(rootPath)
     }
 
     /**
      * @implSpec the map is ordered by length of package
      */
-    private final SortedMap<String, String> relocateMapping = new TreeMap<>(
-            Comparator.comparingInt(String::length).reversed()
-                    .thenComparing(Function.identity()));
+    private val relocateMapping: SortedMap<String, String?> = TreeMap(
+        Comparator.comparingInt { obj: String -> obj.length }.reversed()
+            .thenComparing(Function.identity()))
 
-    public void addRelocateMapping(String relocateFrom, String relocateTo) {
-        requireNonNull(relocateFrom, "relocateFrom");
-        requireNonNull(relocateTo, "relocateTo");
-        if (relocateMapping.putIfAbsent(relocateFrom, relocateTo) != null) {
-            throw new IllegalStateException("relocation for " + relocateFrom + " already exists.");
-        }
+    fun addRelocateMapping(relocateFrom: String, relocateTo: String) {
+        check(relocateMapping.putIfAbsent(relocateFrom,
+            relocateTo) == null) { "relocation for $relocateFrom already exists." }
     }
 }
