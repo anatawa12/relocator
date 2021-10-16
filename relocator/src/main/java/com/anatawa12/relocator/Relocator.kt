979e62fd47c76e4159aee914c6920e0661c1e2a6
anatawa12 <anatawa12@icloud.com>
2021-10-16 17:01:56 +0900

    Add path set for runtime classpath

--- relocator/src/main/java/com/anatawa12/relocator/Relocator.kt
+++ relocator/src/main/java/com/anatawa12/relocator/Relocator.kt
@@ -5,18 +5,38 @@ import java.util.*
 import java.util.function.Function
 
 class Relocator {
-    private val _classPath: MutableList<File> = ArrayList()
+    private val _referPath: MutableList<File> = ArrayList()
+    /**
+     * The list of jar files/directories which will be added 
+     * to runtime classpath.
+     * JDK bootclasspath should be added to this path.
+     * If `java.lang.Object` does not exists on this path,
+     * all files in `$JAVA_HOME/lib`, `$JAVA_HOME/lib/ext`,
+     * `$JAVA_HOME/jre/lib`, and `$JAVA_HOME/jre/lib/ext` will be added.
+     * (`$JAVA_HOME` will be the value of `java.home` system property)
+     */
+    val referPath: List<File> = Collections.unmodifiableList(_referPath)
+
+    /**
+     * Add a file or directory to [Relocator.referPath].
+     * @param referPath The path to file or directory to [Relocator.referPath].
+     */
+    fun addReferPath(referPath: File) {
+        this._referPath.add(referPath)
+    }
+
+    private val _embedPath: MutableList<File> = ArrayList()
     /**
      * The list of jar files/directories which has classes that will be embedded if needed.
      */
-    val classPath: List<File> = Collections.unmodifiableList(_classPath)
+    val embedPath: List<File> = Collections.unmodifiableList(_embedPath)
 
     /**
-     * Add a file or directory to [Relocator.classPath].
-     * @param classPath The path to file or directory to [Relocator.classPath].
+     * Add a file or directory to [Relocator.embedPath].
+     * @param embedPath The path to file or directory to [Relocator.embedPath].
      */
-    fun addClassPath(classPath: File) {
-        this._classPath.add(classPath)
+    fun addEmbedPath(embedPath: File) {
+        this._embedPath.add(embedPath)
     }
 
     private val _rootPath: MutableList<File> = ArrayList()
@@ -28,10 +48,10 @@ class Relocator {
 
     /**
      * Add a file or directory to [Relocator.rootPath].
-     * @param classPath The path to file or directory to [Relocator.rootPath].
+     * @param rootPath The path to file or directory to [Relocator.rootPath].
      */
-    fun addRootClassPath(classPath: File) {
-        _rootPath.add(classPath)
+    fun addRootPath(rootPath: File) {
+        _rootPath.add(rootPath)
     }
 
 
